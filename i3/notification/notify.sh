a071c8a4ca2fedde4fe1a62a605696d327c70a34
Raphael Ehret <ehretrapha@eisti.eu>
2016-11-16 17:29:42 +0100

    add: Detection of layout in i3status + font and color change of i3status

---
+++ i3/notification/notify.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: notify.sh
+# 
+#         USAGE: ./notify.sh 
+# 
+#   DESCRIPTION: Notify users using the provided scripts
+#
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: notify-send
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Raphael EHRET (), raphael[dot]ehret[at]gmail[dot]com
+#       CREATED: 11/15/2016 06:36:35 PM
+#      REVISION:  ---
+#===============================================================================
+
